b180e1d266e767d8b392ce5681364ff0a8f38a1f
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-08-01 19:42:10 +0700

    fix: fix custom style

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,26 @@ module.exports = {
       },
       colors: {
         'threads-bg': '#16161A',
-        'threads-purple': '#7F5AF0',
+        'threads-purple': {
+          50: '#f4f3ff',
+          100: '#eceafd',
+          200: '#dcd7fd',
+          300: '#c1b7fb',
+          400: '#a38ff6',
+          500: '#7f5af0',
+          600: '#7440e7',
+          700: '#642ed3',
+          800: '#5426b1',
+          900: '#462191',
+          950: '#2a1362',
+        },
+
         'threads-white': '#FFFFFE',
         'threads-gray': '#94A1B2',
         'threads-dark': '#242629',
+      },
+      outlineWidth: {
+        0.5: '0.5px',
       },
     },
   },
