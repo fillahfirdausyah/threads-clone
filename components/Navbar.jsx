3c578895756ce2b7de3b736ccc8799038b887c1a
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-08-01 19:46:17 +0700

    fix: refactor and fix navbar style

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,47 +1,111 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { HomeIcon } from './icons/HomeIcon';
+import { NotificationIcon } from './icons/NotificationIcon';
+import { AddIcon } from './icons/AddIcon';
+import { FireIcon } from './icons/FireIcon';
+import { ThreadsLogo } from './icons/ThreadsLogo';
+import { SearchIcon } from './icons/SearchIcon';
 
 const Navbar = () => {
+  const pathname = usePathname();
   return (
     <nav className="w-full">
-      <div className="w-full flex items-center justify-center p-2 border-b-[1px] border-opacity-60 border-b-white">
-        <Image
+      {/* Desktop Nav */}
+      <div className="hidden lg:flex items-center justify-between px-4 py-3 border-b-[1px] border-opacity-60 border-b-white fixed top-0 left-0 right-0 z-10 bg-threads-bg">
+        {/* Brand Image */}
+        <div className="flex items-center gap-2">
+          <ThreadsLogo width={30} height={30} />
+          <h1 className="font-semibold text-threads-white text-lg">
+            Threads Clone
+          </h1>
+        </div>
+
+        {/* Search Input */}
+        <div className="flex items-center my-0 ms-28 max-w-xl">
+          <input
+            type="text"
+            className="bg-threads-dark px-4 py-2 w-[36rem] h-10  text-threads-white rounded-md focus:outline-none"
+            placeholder="Search"
+          />
+        </div>
+
+        {/* Nav Link and User area */}
+        <div className="flex justify-between w-72">
+          <Link href={'/'}>
+            <HomeIcon
+              width={30}
+              height={30}
+              color={pathname === '/' ? '#7F5AF0' : '#fff'}
+            />
+          </Link>
+          <Link href={'/'}>
+            <NotificationIcon
+              width={30}
+              height={30}
+              color={pathname === '/notifications' ? '#7F5AF0' : '#fff'}
+            />
+          </Link>
+          <Link href={'/new-threads'}>
+            <AddIcon width={30} height={30} />
+          </Link>
+          <Link href={'/'}>
+            <FireIcon width={30} height={30} />
+          </Link>
+          <Link href={'/profile/pixelpilot4'}>
+            <Image
+              src={'/Assets/img/user.png'}
+              width={35}
+              height={35}
+              className="rounded-full object-contain"
+            />
+          </Link>
+        </div>
+      </div>
+      {/* Desktop Nav */}
+
+      {/* Mobile Header */}
+      <div className="md:hidden w-full flex items-center justify-center py-6">
+        {/* <Image
           src={'/Assets/img/threads-logo-png.webp'}
           width={30}
           height={30}
-        />
+        /> */}
+        <ThreadsLogo width={30} height={30} />
       </div>
+      {/* Mobile Header */}
 
-      <div className="fixed z-10 bottom-0 right-0 left-0 w-full p-4 sm:hidden flex border-t-[1px] border-opacity-60 justify-between border-t-white bg-threads-bg">
+      {/* Mobile Nav */}
+      <div className="md:hidden fixed z-10 bottom-0 right-0 left-0 w-full p-4  flex border-t-[1px] border-opacity-60 justify-between border-t-white bg-threads-bg">
         <Link href={'/'}>
-          <Image
-            src={'/Assets/icon/Outline/General/Home.svg'}
-            width={35}
-            height={35}
+          <HomeIcon
+            width={30}
+            height={30}
+            color={pathname === '/' ? '#7F5AF0' : '#fff'}
           />
         </Link>
         <Link href={'/'}>
-          <Image
-            src={'/Assets/icon/Outline/Status/Notification.svg'}
-            width={35}
-            height={35}
+          <SearchIcon
+            width={30}
+            height={30}
+            color={pathname === '/search' ? '#7F5AF0' : '#fff'}
           />
         </Link>
         <Link href={'/new-threads'}>
-          <Image
-            src={'/Assets/icon/Outline/Interface/Add.svg'}
-            width={35}
-            height={35}
+          <AddIcon width={30} height={30} />
+        </Link>
+        <Link href={'/notifications'}>
+          <NotificationIcon
+            width={30}
+            height={30}
+            color={pathname === '/notifications' ? '#7F5AF0' : '#fff'}
           />
         </Link>
-        <Link href={'/'}>
-          <Image
-            src={'/Assets/icon/Outline/Status/Fire.svg'}
-            width={35}
-            height={35}
-          />
-        </Link>
-        <Link href={'/'}>
+        <Link href={'/profile/pixelpilot4'}>
           <Image
             src={'/Assets/img/user.png'}
             width={35}
@@ -50,6 +114,7 @@ const Navbar = () => {
           />
         </Link>
       </div>
+      {/* Mobile Nav */}
     </nav>
   );
 };
