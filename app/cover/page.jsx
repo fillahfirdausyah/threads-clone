8a7fdfb55e094352c47d7fb7d14ea938c5f4c4b0
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-08-02 23:03:39 +0700

    fix: refactor style cover page

--- app/cover/page.jsx
+++ app/cover/page.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
+'use client';
+
+import { signIn } from 'next-auth/react';
 
 import { ThreadsLogo } from '@components/icons/ThreadsLogo';
 
 const CoverPage = () => {
   return (
-    <section className="cover min-h-screen max-w-xl mx-auto px-6 mt-3 text-center text-threads-white">
-      <div className="flex items-center justify-center mt-16">
-        <ThreadsLogo width={50} height={50} />
-      </div>
-      <h1 className="mt-32 text-[50px]">Welcome To Threads Clone</h1>
-      <p className="mt-16 text-[20px] px-20 text-threads-gray">
-        Let’s share what is in your mind with text based Social Media
-      </p>
-      <button className="bg-threads-purple-500 w-40 h-10 px-3 py-1 rounded-md text-threads-white text-sm font-medium hover:bg-threads-purple-400 mt-16">
-        Let's Join
-      </button>
-    </section>
+    <>
+      <section className="cover min-h-screen max-w-xl mx-auto px-6 mt-2 text-center text-threads-white">
+        <div className="flex items-center justify-center mt-10">
+          <ThreadsLogo width={50} height={50} />
+        </div>
+        <h1 className="mt-32 text-[50px]">Welcome To Threads Clone</h1>
+        <p className="mt-16 text-[20px] px-20 text-threads-gray">
+          Let’s share what is in your mind with text based Social Media
+        </p>
+
+        <button
+          type="button"
+          onClick={() => signIn('credentials')}
+          className="bg-threads-purple-500 w-40 h-10 px-3 py-1 rounded-md text-threads-white text-sm font-medium hover:bg-threads-purple-400 mt-16"
+        >
+          Let's Join
+        </button>
+      </section>
+    </>
   );
 };
 
