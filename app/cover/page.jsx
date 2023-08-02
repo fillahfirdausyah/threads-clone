0a2bfc0cbadb0c543bb5f348e8c1607334324600
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-08-02 12:09:38 +0700

    fix: finished cover page

--- app/cover/page.jsx
+++ app/cover/page.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import { ThreadsLogo } from '@components/icons/ThreadsLogo';
+
 const CoverPage = () => {
-  return <div>CoverPage</div>;
+  return (
+    <section className="cover min-h-screen max-w-xl mx-auto px-6 mt-3 text-center text-threads-white">
+      <div className="flex items-center justify-center mt-16">
+        <ThreadsLogo width={50} height={50} />
+      </div>
+      <h1 className="mt-32 text-[50px]">Welcome To Threads Clone</h1>
+      <p className="mt-16 text-[20px] px-20 text-threads-gray">
+        Let’s share what is in your mind with text based Social Media
+      </p>
+      <button className="bg-threads-purple-500 w-40 h-10 px-3 py-1 rounded-md text-threads-white text-sm font-medium hover:bg-threads-purple-400 mt-16">
+        Let's Join
+      </button>
+    </section>
+  );
 };
 
 export default CoverPage;
