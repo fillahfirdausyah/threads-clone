9a9625b5014271a9a936ff1e0f1d077fe7a3c873
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-08-02 12:12:46 +0700

    fix: add header section

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,12 @@
 import Feed from '@components/Feed';
 
+import Header from '@components/Header';
+
 const HomePage = () => {
   return (
     <main className="text-threads-white">
-      <div className="flex max-w-xl mx-auto ">
+      <Header />
+      <div className="flex max-w-xl mx-auto relative">
         <Feed />
       </div>
     </main>
