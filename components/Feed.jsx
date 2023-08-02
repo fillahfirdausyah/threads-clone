a6c242db8904c804951a0b79a934dadc185cb61a
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-08-02 23:05:58 +0700

    fix: get threads data from api and render usign PostCardList component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,87 +1,36 @@
+'use client';
+
 import PostCard from './PostCard';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+
+const PostCardList = () => {
+  const { data: session } = useSession();
+  const [threads, setThreads] = useState([]);
+
+  useEffect(() => {
+    const getThreads = async () => {
+      const response = await fetch('http://localhost:5000/v1/threads');
+      const data = await response.json();
+      setThreads(data.data);
+    };
+
+    session?.user && getThreads();
+  }, []);
+
+  return (
+    <div className="w-full mb-16 lg:mt-28">
+      {threads.map((thread) => (
+        <PostCard key={thread.id} post={thread} />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
   return (
     <div className="w-full mb-16 lg:mt-28">
-      <PostCard
-        post={{
-          username: 'PixelPilot4',
-          time: '2h',
-          posts: {
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-            repliesCount: 10,
-          },
-        }}
-      />
-      <PostCard
-        post={{
-          username: 'PixelPilot4',
-          time: '2h',
-          posts: {
-            text: 'Lorem ipsum dolor sit ame',
-            image: '/Assets/img/post1.png',
-            repliesCount: 10,
-            likesCount: 80,
-          },
-        }}
-      />
-      <PostCard
-        post={{
-          username: 'PixelPilot4',
-          time: '2h',
-          posts: {
-            text: 'Lorem ipsum dolor sit ame',
-          },
-        }}
-      />
-      <PostCard
-        post={{
-          username: 'PixelPilot4',
-          time: '2h',
-          posts: {
-            text: 'Lorem ipsum dolor sit ame',
-            image: '/Assets/img/post2.png',
-            repliesCount: 1,
-            likesCount: 80,
-          },
-        }}
-      />
-      <PostCard
-        post={{
-          username: 'PixelPilot4',
-          time: '2h',
-          posts: {
-            text: 'Lorem ipsum dolor sit ame',
-          },
-        }}
-      />
-      <PostCard
-        post={{
-          username: 'PixelPilot4',
-          time: '2h',
-          posts: {
-            text: 'Lorem ipsum dolor sit ame',
-          },
-        }}
-      />
-      <PostCard
-        post={{
-          username: 'PixelPilot4',
-          time: '2h',
-          posts: {
-            text: 'Lorem ipsum dolor sit ame',
-          },
-        }}
-      />
-      <PostCard
-        post={{
-          username: 'PixelPilot4',
-          time: '2h',
-          posts: {
-            text: 'Lorem ipsum dolor sit ame',
-          },
-        }}
-      />
+      <PostCardList />
     </div>
   );
 };
