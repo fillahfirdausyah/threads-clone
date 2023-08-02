fa7be20f1d376a8b6b9b3cbb599530432c7ff0f4
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-08-02 12:13:16 +0700

    fix: check if has session render home page else cover page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,27 @@
-'use client';
 import '@styles/global.css';
-import { usePathname } from 'next/navigation';
 import Navbar from '@components/Navbar';
+
+import CoverPage from './cover/page';
 
 export const metadata = {
   title: 'Threads Clone',
   description: 'Let’s share what is in your mind with text based Social Media',
 };
 
+const session = true;
+
 const RootLayout = ({ children }) => {
-  const pathname = usePathname();
   return (
     <html lang="en" className="bg-threads-bg">
       <body className="font-lato">
-        {pathname === '/new-threads' ? null : <Navbar />}
-        <div className="px-2 relative">{children}</div>
+        {session ? (
+          <>
+            <Navbar />
+            <div className="px-2 relative">{children}</div>
+          </>
+        ) : (
+          <CoverPage />
+        )}
       </body>
     </html>
   );
