0ef2a204362f41cae5735ae929708a12ce082714
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-08-04 16:10:44 +0700

    fix: refactor structut and check if user loged in or not

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,27 +1,21 @@
 import '@styles/global.css';
 import Navbar from '@components/Navbar';
 
-import CoverPage from './cover/page';
+import Provider from '@components/Provider';
 
 export const metadata = {
   title: 'Threads Clone',
   description: 'Let’s share what is in your mind with text based Social Media',
 };
 
-const session = true;
-
 const RootLayout = ({ children }) => {
   return (
     <html lang="en" className="bg-threads-bg">
       <body className="font-lato">
-        {session ? (
-          <>
-            <Navbar />
-            <div className="px-2 relative">{children}</div>
-          </>
-        ) : (
-          <CoverPage />
-        )}
+        <Provider>
+          <Navbar />
+          <div className="px-2 relative">{children}</div>
+        </Provider>
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,26 @@
+'use client';
 import Feed from '@components/Feed';
 
 import Header from '@components/Header';
 
+import { useSession } from 'next-auth/react';
+import CoverPage from './cover/page';
+
 const HomePage = () => {
+  const { data: session } = useSession();
   return (
-    <main className="text-threads-white">
-      <Header />
-      <div className="flex max-w-xl mx-auto relative">
-        <Feed />
-      </div>
-    </main>
+    <>
+      {session?.user ? (
+        <main className="text-threads-white">
+          <Header />
+          <div className="flex max-w-xl mx-auto relative">
+            <Feed />
+          </div>
+        </main>
+      ) : (
+        <CoverPage />
+      )}
+    </>
   );
 };
 
