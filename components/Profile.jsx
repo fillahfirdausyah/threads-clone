ca16a8880e1181551ae4f063d988bc96ccd3dd6f
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-08-06 17:01:32 +0700

    feat: make profile component

---
+++ components/Profile.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Profile = ({ children, userId, session, bio, link }) => {
+  return (
+    <section className="mt-4 max-w-xl mx-auto px-3 lg:mt-28 text-threads-white">
+      <div className="flex w-full justify-between items-start">
+        <div className="flex flex-col">
+          <h1 className="font-bold text-2xl">
+            {session?.user.fullname
+              ? session?.user.fullname
+              : session?.user.name}
+          </h1>
+          <p>
+            @{session?.user.username}
+            <span className="bg-threads-dark px-2 rounded-full text-sm text-threads-gray">
+              threadsclone.net
+            </span>
+          </p>
+          {bio && <p className="mt-4">{bio}</p>}
+        </div>
+        <Image
+          src={session?.user.image}
+          width={60}
+          height={60}
+          className="rounded-full object-contain"
+        />
+      </div>
+      <div className="flex items-center justify-between mt-5 text-threads-gray">
+        <div className="flex gap-1 text-sm">
+          <p>123 Followers</p>
+          <span className="text-threads-gray">•</span>
+          {link && (
+            <p className="cursor-pointer">
+              <Link href={`http://${link}`} target="_blank">
+                {link}
+              </Link>
+            </p>
+          )}
+        </div>
+        <div className="bg-transparent outline outline-2 rounded-full">
+          <Image
+            src={'/Assets/icon/Outline/Interface/Other-1.svg'}
+            width={25}
+            height={25}
+            className="cursor-pointer"
+          />
+        </div>
+      </div>
+      <div className="w-full flex gap-6 text-center mt-5">
+        {session?.user.username === userId && (
+          <>
+            <Link
+              href={`/profile/${session?.user.username}/setting`}
+              className="bg-transparent outline outline-1 text-sm py-[0.4rem] px-7 rounded-md w-1/2 hover:bg-threads-purple-500 hover:outline-threads-purple-500"
+            >
+              Edit Profile
+            </Link>
+            <button className="bg-transparent outline outline-1 text-sm py-[0.4rem] px-7 rounded-md w-1/2 hover:bg-threads-purple-500 hover:outline-threads-purple-500">
+              Share Profile
+            </button>
+          </>
+        )}
+        {session?.user.username !== userId && (
+          <>
+            <button className="bg-threads-purple-500 py-1 px-7 rounded-md w-full hover:bg-threads-purple-400">
+              Follow
+            </button>
+            <button className="bg-transparent outline outline-1 py-1 px-7 rounded-md w-full hover:bg-threads-purple-500 hover:outline-threads-purple-500">
+              Following
+            </button>
+          </>
+        )}
+      </div>
+      <div className="mt-5 flex items-center text-center justify-between text-threads-gray gap-1">
+        <div className="flex-1 py-3 tab-active cursor-pointer">
+          <h1>Threads</h1>
+        </div>
+        <div className="flex-1 py-3 cursor-pointer border-b-[0.5px] border-b-threads-gray">
+          <h1>Replies</h1>
+        </div>
+      </div>
+      <section>{children}</section>
+    </section>
+  );
+};
+
+export default Profile;
