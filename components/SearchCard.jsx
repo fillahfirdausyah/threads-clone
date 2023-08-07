e23f933a349e2ba0bf55f49fb2f2d27da636ad5a
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-08-07 19:21:38 +0700

    fix: display userdata to card, and store clicked user to recent search in localstorage

--- components/SearchCard.jsx
+++ components/SearchCard.jsx
@@ -1,18 +1,44 @@
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
-const SearchCard = () => {
+const SearchCard = ({ user }) => {
+  const router = useRouter();
+  const toProfile = () => {
+    const recentSearch = JSON.parse(localStorage.getItem('recent-search'));
+    if (recentSearch === null) {
+      localStorage.setItem('recent-search', JSON.stringify(user));
+    } else {
+      const newRecentSearch = recentSearch._id ? [recentSearch] : recentSearch;
+      if (
+        newRecentSearch.find((x) => x.username === user.username) === undefined
+      ) {
+        newRecentSearch.push(user);
+        localStorage.setItem('recent-search', JSON.stringify(newRecentSearch));
+      }
+    }
+    router.push(`/profile/${user.username}`);
+  };
+
   return (
     <div className="flex items-start gap-2 text-threads-white post-separator py-2">
       <Image
-        src={'/Assets/img/user.png'}
+        onClick={toProfile}
+        src={user.image}
         width={40}
         height={40}
-        className="rounded-full object-contain"
+        className="rounded-full object-contain cursor-pointer"
       />
       <div className="flex flex-1 justify-between  items-center">
         <div className="flex flex-col">
-          <h1 className="font-semibold">pixelpilot4</h1>
-          <h4 className="text-sm text-threads-gray">Barry Allen</h4>
+          <h1 onClick={toProfile} className="font-semibold cursor-pointer">
+            {user.username}
+          </h1>
+          <h4
+            onClick={toProfile}
+            className="text-sm text-threads-gray cursor-pointer"
+          >
+            {user.fullname}
+          </h4>
         </div>
         <button className="bg-threads-purple-500 px-3 py-1 rounded-md text-threads-white text-sm font-medium hover:bg-threads-purple-400">
           Follow
