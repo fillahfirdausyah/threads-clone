0e4345dc106002ab895ce6b17e0afbcd9de5926c
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-08-07 19:20:25 +0700

    feat: make functionality to search other users, and implement debounce for search users

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,7 +1,49 @@
-import React from 'react';
+'use client';
+
+import { useState, useEffect } from 'react';
 import SearchCard from '@components/SearchCard';
+import { debounce } from '@utils/debounce';
+
+const SearchList = ({ data }) => {
+  return (
+    <>
+      {data.map((user) => (
+        <SearchCard key={user.id} user={user} />
+      ))}
+    </>
+  );
+};
 
 const SearchPage = () => {
+  const [search, setSearch] = useState('');
+  const [debounceTimeoutId, setDebounceTimeoutId] = useState(null);
+  const [result, setResult] = useState([]);
+  const [recentSearch, setRecentSearch] = useState([]);
+
+  const performSearch = async () => {
+    const response = await fetch(
+      `http://localhost:5000/v1/users?username=${search}`
+    );
+    const data = await response.json();
+    setResult(data.data);
+  };
+
+  const getRecentSearch = async () => {
+    const recentSearch = JSON.parse(localStorage.getItem('recent-search'));
+    setRecentSearch(recentSearch);
+  };
+
+  useEffect(() => {
+    if (debounceTimeoutId) clearTimeout(debounceTimeoutId);
+
+    const newDounceTimeoutId = debounce(() => {
+      if (search === '') return setResult([]);
+      performSearch();
+    }, 500);
+
+    setDebounceTimeoutId(newDounceTimeoutId);
+  }, [search]);
+
   return (
     <section className="mt-4 max-w-xl mx-auto px-3 mb-16 lg:mt-28 overflow-x-hidden">
       <div className="flex items-center">
@@ -12,19 +54,30 @@ const SearchPage = () => {
       </div>
       <div className="w-full mt-2">
         <input
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+          name="search"
           type="text"
           className="w-full h-9 px-2 bg-threads-dark text-threads-white rounded-md focus:outline-none"
           placeholder="Search"
         />
       </div>
-      <div className="mt-5">
-        <SearchCard />
-        <SearchCard />
-        <SearchCard />
-        <SearchCard />
-        <SearchCard />
-        <SearchCard />
-      </div>
+      {search === '' ? (
+        <>
+          <div>
+            <h1 className="text-threads-white text-lg font-semibold mt-5">
+              Recent Searches
+            </h1>
+            <div className="mt-5">
+              <SearchList data={recentSearch} />
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="mt-5">
+          <SearchList data={result} />
+        </div>
+      )}
     </section>
   );
 };
