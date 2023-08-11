71323ffb09092b681f0630f27c8003ec1aeb87c5
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-08-11 15:44:43 +0700

    feat: make connection to backend socket when connection succes trigger register user to socket. and make event to get updated notifications

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,31 @@
 'use client';
-import Feed from '@components/Feed';
 
 import Header from '@components/Header';
+import CoverPage from './cover/page';
+import Feed from '@components/Feed';
 
 import { useSession } from 'next-auth/react';
-import CoverPage from './cover/page';
+import { useEffect } from 'react';
+import { socket } from '@utils/socket';
+import { useNotifications } from '@utils/context/notificationsContext';
 
 const HomePage = () => {
   const { data: session } = useSession();
+  const { addNotificationCounter, notificationsCounter } = useNotifications();
+
+  useEffect(() => {
+    if (session?.user) {
+      socket.connect();
+      socket.on('connect', () => {
+        socket.emit('registerUserToSocket', session?.user.id);
+        console.log('connected');
+      });
+      socket.on(`notification:${session?.user.id}`, () => {
+        addNotificationCounter();
+      });
+    }
+  }, [session, notificationsCounter]);
+
   return (
     <>
       {session?.user ? (
