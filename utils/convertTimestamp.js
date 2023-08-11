8d6045b514d4c35764a3f780f7719e2e0cf68ad2
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-08-11 15:42:40 +0700

    fix<util>: fix covenbert timestamp utils to more readable format

--- utils/convertTimestamp.js
+++ utils/convertTimestamp.js
@@ -1,10 +1,35 @@
 export const convertTimestamp = (timestamp) => {
-  const date = new Date(timestamp);
-  const oneHourAgo = new Date(date.getTime() - 3600000);
+  // Waktu saat ini dalam format ISO
+  const waktuSaatIni = new Date();
 
-  const formattedDate = oneHourAgo.toLocaleString('en-US', {
-    hour12: false,
-  });
+  // Waktu yang akan diubah
+  const waktuTarget = new Date(timestamp); // Ganti dengan waktu yang diinginkan
 
-  return formattedDate;
+  // Menghitung selisih waktu dalam milidetik
+  const selisihWaktu = waktuSaatIni - waktuTarget;
+
+  // Menghitung selisih hari, jam, dan menit
+  const selisihHari = Math.floor(selisihWaktu / (1000 * 60 * 60 * 24));
+  const selisihJam = Math.floor(selisihWaktu / (1000 * 60 * 60));
+  const selisihMenit = Math.floor(
+    (selisihWaktu % (1000 * 60 * 60)) / (1000 * 60)
+  );
+
+  // Menghasilkan teks berdasarkan selisih waktu
+  let formatWaktu = '';
+  if (selisihHari === 0) {
+    if (selisihJam === 0 && selisihMenit === 0) {
+      formatWaktu = 'Now';
+    } else if (selisihJam === 0) {
+      formatWaktu = `${selisihMenit}m`;
+    } else {
+      formatWaktu = `${selisihJam}h`;
+    }
+  } else if (selisihHari === 1) {
+    formatWaktu = '1d';
+  } else {
+    formatWaktu = `${selisihHari}d`;
+  }
+
+  return formatWaktu;
 };
