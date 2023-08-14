8683bdb5f46e6af82f617110831fa668224e4117
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-08-14 14:04:03 +0700

    fix: fix get data and rnder notification card by notifications type

--- app/notifications/page.jsx
+++ app/notifications/page.jsx
@@ -1,20 +1,25 @@
-'use client';
-import NotificationsCard from '@components/NotificationsCard';
+"use client";
+import NotificationsCard from "@components/NotificationsCard";
+import Navbar from "@components/Navbar";
 
-import { useNotifications } from '@utils/context/notificationsContext';
-import { useState, useEffect } from 'react';
-import { useSession } from 'next-auth/react';
+import { useNotifications } from "@utils/context/notificationsContext";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
 
 const NotificationsPage = () => {
   const { data: session } = useSession();
   const { removeAllNotificationsCounter, notificationsCounter } =
     useNotifications();
-  const [notifications, setNotifications] = useState([]);
+  const [notifications, setNotifications] = useState({
+    follow: [],
+    like: [],
+    comments: [],
+  });
 
   useEffect(() => {
     const getNotifications = async () => {
       const response = await fetch(
-        `http://localhost:5000/v1/notifications?user_id=${session?.user.id}`
+        `http://localhost:5000/v1/notifications?user_id=${session?.user.id}`,
       );
       const data = await response.json();
       setNotifications(data.data);
@@ -25,25 +30,31 @@ const NotificationsPage = () => {
   }, [session, notificationsCounter]);
 
   return (
-    <section className="mt-4 max-w-xl mx-auto px-3 mb-16 lg:mt-28 overflow-x-hidden">
-      <div className="flex items-center">
-        <h1 className="text-threads-white text-2xl font-semibold me-1">
-          Notifications Center
-        </h1>
-        <p>🔔</p>
-      </div>
-      <div className="mt-5">
-        {notifications.map((notification) => (
-          <NotificationsCard
-            key={notification.id}
-            notifications={notification}
-          />
-        ))}
-        {/* <NotificationsCard notifications={{ type: 'likes', likesCount: 3 }} />
-        <NotificationsCard notifications={{ type: 'comments' }} />
-        <NotificationsCard notifications={{ type: 'follower' }} /> */}
-      </div>
-    </section>
+    <>
+      <Navbar />
+      <section className="mx-auto mb-16 mt-4 max-w-xl overflow-x-hidden px-3 lg:mt-28">
+        <div className="flex items-center">
+          <h1 className="me-1 text-2xl font-semibold text-threads-white">
+            Notifications Center
+          </h1>
+          <p>🔔</p>
+        </div>
+        <div className="mt-5">
+          {notifications.follow.map((notification) => (
+            <NotificationsCard
+              key={notification._id}
+              data={{ type: "follow", notification }}
+            />
+          ))}
+          {notifications.like.map((notification) => (
+            <NotificationsCard
+              key={notification._id}
+              data={{ type: "follow", notification }}
+            />
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
 
