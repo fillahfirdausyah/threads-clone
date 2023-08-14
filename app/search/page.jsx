eb9a70e7ed8e2cb9224b1d9b77cd4b404251d0c8
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-08-14 14:06:48 +0700

    fix: refactor, use navbar

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from 'react';
 import SearchCard from '@components/SearchCard';
 import { debounce } from '@utils/debounce';
+import Navbar from '@components/Navbar';
 
 const SearchList = ({ filterRecentSearch, type, data }) => {
   return (
@@ -69,44 +70,47 @@ const SearchPage = () => {
   }, [search]);
 
   return (
-    <section className="mt-4 max-w-xl mx-auto px-3 mb-16 lg:mt-28 overflow-x-hidden">
-      <div className="flex items-center">
-        <h1 className="text-threads-white text-2xl font-semibold me-1">
-          Search
-        </h1>
-        <p>🔍</p>
-      </div>
-      <div className="w-full mt-2">
-        <input
-          onChange={(e) => setSearch(e.target.value)}
-          value={search}
-          name="search"
-          type="text"
-          className="w-full h-9 px-2 bg-threads-dark text-threads-white rounded-md focus:outline-none"
-          placeholder="Search"
-        />
-      </div>
-      {search === '' ? (
-        <>
-          <div>
-            <h1 className="text-threads-white text-lg font-semibold mt-5">
-              Recent Searches
-            </h1>
-            <div className="mt-5">
-              <SearchList
-                filterRecentSearch={filterRecentSearch}
-                type={'RecentSearch'}
-                data={recentSearch}
-              />
-            </div>
-          </div>
-        </>
-      ) : (
-        <div className="mt-5">
-          <SearchList data={result} />
+    <>
+      <Navbar />
+      <section className="mt-4 max-w-xl mx-auto px-3 mb-16 lg:mt-28 overflow-x-hidden">
+        <div className="flex items-center">
+          <h1 className="text-threads-white text-2xl font-semibold me-1">
+            Search
+          </h1>
+          <p>🔍</p>
         </div>
-      )}
-    </section>
+        <div className="w-full mt-2">
+          <input
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+            name="search"
+            type="text"
+            className="w-full h-9 px-2 bg-threads-dark text-threads-white rounded-md focus:outline-none"
+            placeholder="Search"
+          />
+        </div>
+        {search === '' ? (
+          <>
+            <div>
+              <h1 className="text-threads-white text-lg font-semibold mt-5">
+                Recent Searches
+              </h1>
+              <div className="mt-5">
+                <SearchList
+                  filterRecentSearch={filterRecentSearch}
+                  type={'RecentSearch'}
+                  data={recentSearch}
+                />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="mt-5">
+            <SearchList data={result} />
+          </div>
+        )}
+      </section>
+    </>
   );
 };
 
