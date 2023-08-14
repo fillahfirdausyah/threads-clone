ed8fd45c678d19e4e02a661abf3a9fd50c6c9faa
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2023-08-14 14:10:42 +0700

    fix: add header separator style

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <>
       {/* Mobile Header */}
-      <div className="md:hidden w-full flex items-center justify-center py-6">
+      <div className="md:hidden header-separator w-full flex items-center justify-center py-6">
         <ThreadsLogo width={30} height={30} />
       </div>
       {/* Mobile Header */}
