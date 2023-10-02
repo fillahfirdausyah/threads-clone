a886b177653d84c27da404058b5cb939d29f99f3
fillah firdausyah <fillah.akbar@viapulsa.id>
2023-10-02 17:46:31 +0700

    fix: make back button to back in url history

--- components/HeaderNav.jsx
+++ components/HeaderNav.jsx
@@ -1,22 +1,25 @@
-import Image from 'next/image';
-import Link from 'next/link';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const HeaderNav = ({ title, type }) => {
+  const router = useRouter();
   return (
-    <nav className="bg-threads-bg z-10 max-w-xl mx-auto fixed top-0 right-0 left-0">
-      <div className="w-full flex items-center justify-start gap-4 p-3 header-separator">
-        <Link href={'/'} replace>
-          <Image
-            src={
-              type === 'back'
-                ? '/Assets/icon/Outline/Interface/Arrow left.svg'
-                : '/Assets/icon/Outline/Interface/Cross.svg'
-            }
-            width={30}
-            height={30}
-          />
-        </Link>
-        <h2 className="text-threads-white text-base font-semibold">{title}</h2>
+    <nav className="fixed left-0 right-0 top-0 z-10 mx-auto max-w-xl bg-threads-bg">
+      <div className="header-separator flex w-full items-center justify-start gap-4 p-3">
+        <Image
+          onClick={() => router.back()}
+          src={
+            type === "back"
+              ? "/Assets/icon/Outline/Interface/Arrow left.svg"
+              : "/Assets/icon/Outline/Interface/Cross.svg"
+          }
+          width={30}
+          height={30}
+        />
+
+        <h2 className="text-base font-semibold text-threads-white">{title}</h2>
       </div>
     </nav>
   );
