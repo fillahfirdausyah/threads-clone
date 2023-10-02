d3d09e52a6b89076ce2b7aa080d6d0310032ae9f
fillah firdausyah <fillah.akbar@viapulsa.id>
2023-10-02 17:45:58 +0700

    fix: add message button in mobile header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import { ThreadsLogo } from './icons/ThreadsLogo';
+import { ThreadsLogo } from "./icons/ThreadsLogo";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <>
       {/* Mobile Header */}
-      <div className="md:hidden header-separator w-full flex items-center justify-center py-6">
-        <ThreadsLogo width={30} height={30} />
+      <div className="header-separator flex w-full items-center justify-between px-2 py-6 md:hidden">
+        <div className="flex items-center justify-center gap-1 text-white">
+          <ThreadsLogo width={30} height={30} />
+          <h2>Trheads Clone.</h2>
+        </div>
+        <Link href={"/message"}>
+          <Image
+            src={"/Assets/icon/Outline/Communication/Envelope.svg"}
+            width={30}
+            height={30}
+          />
+        </Link>
       </div>
       {/* Mobile Header */}
     </>
